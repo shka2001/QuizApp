11df095ca9f49d94928c783c82c90fbf573d855a
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-10-06 14:49:10 +0200

    Add class in endjs

--- end.js
+++ end.js
@@ -1,5 +1,10 @@
-document.addEventListener("DOMContentLoaded", function (e) {
-  //const finalScore = document.getElementById("score");
-  playersUsername.innerText = localStorage.getItem("username");
-  finalScore.innerText = localStorage.getItem("playerScore");
-});
+class End {
+  constructor() {
+    document.addEventListener("DOMContentLoaded", function (e) {
+      playersUsername.innerText = localStorage.getItem("username");
+      finalScore.innerText = localStorage.getItem("playerScore");
+    });
+  }
+}
+
+new End();
